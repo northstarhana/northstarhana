5cdee9f27e5022af9875e25d6c24ac174a3ae9c4
Marshall <7885494+mars-cya@users.noreply.github.com>
2024-10-21 02:49:55 -0400

    Quartz sync: Oct 21, 2024, 2:49 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "NORTHSTARHANA DEV BLOG",
+    pageTitle: "Northstar Dev Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
